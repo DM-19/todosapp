98b1665c49f210a6cde509c796c672b9c60ac5b2
Daniel De Hoyos <dsdehoyos@gmail.com>
2019-01-20 15:57:05 -0600

    finishing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent";
+// import Footer from "./components/Footer";
+import TodoItem from "./components/TodoItem";
+import todosData from "./todosData";
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: todosData
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(id) {
+    console.log("changed", id);
+  }
+
   render() {
-    return (
-      <div className="App">
-        <Header />
-        <MainContent />
-        <Footer />
-        <br />
-        <br />
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
-
-        <input type="checkbox" />
-        <p>Placeholder text here</p>
-
-        <input type="checkbox" />
-        <p>lorem ipsum Placeholder text here</p>
-
-        <input type="checkbox" />
-        <p>lorem ipsum Placeholder text here</p>
-      </div>
-    );
+    const todoitems = this.state.todos.map(item => (
+      <TodoItem key={item.id} item={item} handleChange={this.handleChange} />
+    ));
+    return <div className="App">{todoitems}</div>;
   }
 }
 
---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+  {
+    id: 1,
+    text: "Take out the trash wei",
+    completed: true
+  },
+  {
+    id: 2,
+    text: "Grocery Shopping",
+    completed: false
+  },
+  {
+    id: 3,
+    text: "code",
+    completed: false
+  },
+  {
+    id: 4,
+    text: "Mow lawn",
+    completed: true
+  },
+  {
+    id: 5,
+    text: "Catch up on more coding ",
+    completed: false
+  }
+];
+
+export default todosData;
